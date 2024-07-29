bc5a5eb2f776be3820199bb734bccae46b6f5fec
frankrazzih <francischege602@gmail.com>
2024-07-30 02:17:12 +0300

    added more features to the login component

--- app/src/components/Login.tsx
+++ app/src/components/Login.tsx
@@ -1,48 +1,68 @@
 import { useState } from "react";
-import axios from 'axios'
+import axios from "axios";
 
-function Login(){
-    //states to manage input values
-    const [email, ChangeEmail] = useState('')
-    const [pwd, ChangePwd] = useState('')
-    //data object
-    const data = {email, pwd}
-    //handle form submission
-    const HandleSubmit = (e) => {
-        //prevent reload
-        e.preventDefault();
-        //send data to the backend
-        axios.post('http://127.0.0.1:5000/login', JSON.stringify(data), {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(res => console.log(res))
-        .catch(error => console.log(console.log(error)
-        ))
-    };
-    return (
-        <div>
-            <form onSubmit={ HandleSubmit }>
-                <label>Email</label>
-                <input 
-                type="text"
-                value={email}
-                onChange={(e) => ChangeEmail(e.target.value)}
-                />
-
-                <label>Password</label>
-                <input 
-                type="password"
-                value={pwd}
-                onChange={(e) => ChangePwd(e.target.value)} />
-
-                <button type="submit">Login</button>
-            </form>
-            <p>{ email }</p>
-            <p>{ pwd }</p>
+function Login() {
+  //states to manage input values
+  const [email, ChangeEmail] = useState("");
+  const [pwd, ChangePwd] = useState("");
+  const [role, ChangeRole] = useState("patient");
+  //data object
+  const data = { email, pwd };
+  //handle form submission
+  const HandleSubmit = (e) => {
+    //prevent reload
+    e.preventDefault();
+    //send data to the backend
+    axios
+      .post(`http://127.0.0.1:5000/${role}/login`, JSON.stringify(data), {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => console.log(res))
+      .catch((error) => console.log(console.log(error)));
+  };
+  return (
+    <div>
+      <form onSubmit={HandleSubmit}>
+        <h3>Enter your details to login</h3>
+        <div className="mb-3">
+          <label className="form-label">I am a</label>
+          <select
+            className="form-select"
+            onChange={(e) => ChangeRole(e.target.value)}
+          >
+            <option value="user">Patient</option>
+            <option value="doctor">Doctor</option>
+            <option value="hospital">Hospital admin</option>
+            <option value="pharmacy">Pharmacy admin</option>
+          </select>
         </div>
-    )
+        <div className="mb-3">
+          <label className="form-label">Email</label>
+          <input
+            className="form-control"
+            type="email"
+            value={email}
+            onChange={(e) => ChangeEmail(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label className="form-label">Password</label>
+          <input
+            className="form-control"
+            type="password"
+            value={pwd}
+            onChange={(e) => ChangePwd(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary ">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
--- app/src/components/NavBar.tsx
+++ app/src/components/NavBar.tsx
@@ -1,15 +1,25 @@
 //navigation bar
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-function NavBar(){
-    return (
-      <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/login'>Login</Link>
-        <Link to='/booking'>Book Consultation</Link>
-        <Link to='/buyMeds'>Buy medicine</Link>
-      </nav>
-    )
+function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          Home
+        </Link>
+        <Link className="navbar-brand" to="/login">
+          Login
+        </Link>
+        <Link className="navbar-brand" to="/booking">
+          Book Consultation
+        </Link>
+        <Link className="navbar-brand" to="/buyMeds">
+          Buy medicine
+        </Link>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
