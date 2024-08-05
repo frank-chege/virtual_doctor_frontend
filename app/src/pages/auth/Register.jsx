a20223dffcb9d81460dc9593cb69ed1213e1c8b6
frankrazzih <francischege602@gmail.com>
2024-08-06 00:11:08 +0300

    updated the registration form

--- app/src/pages/auth/Register.jsx
+++ app/src/pages/auth/Register.jsx
@@ -1,26 +1,43 @@
 //manages registration
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
 function Register() {
-  const [role, ChangeRole] = useState("patient");
-  const [name, ChangeName] = useState("");
-  const [email, ChangeEmail] = useState("");
-  const [pwd, ChangePwd] = useState("");
-  const [contact, ChangeContact] = useState("");
-  const [birthday, ChangeBirthday] = useState("");
-  const [gender, ChangeGender] = useState("");
-  const [confirmPwd, ChangeConfirmPwd] = useState("");
+  const [role, changeRole] = useState("patient");
+  const [name, changeName] = useState("");
+  const [email, changeEmail] = useState("");
+  const [pwd, changePwd] = useState("");
+  const [contact, changeContact] = useState("");
+  const [birthday, changeBirthday] = useState("");
+  const [gender, changeGender] = useState("");
+  const [confirmPwd, changeConfirmPwd] = useState("");
+  const [file, changeFile] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //check if passwords match
+    if (role === "patient" && pwd !== confirmPwd) {
+      alert("Passwords do not match");
+    }
+    const payload = { role, name, email, contact };
+    if (role != "patient") {
+      payload.pwd = pwd;
+      payload.birthday = birthday;
+      payload.gender = gender;
+    } else {
+      payload.files;
+    }
+  };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label className="form-label">Register as: </label>
         <select
           className="form-select"
           value={role}
-          onChange={(e) => ChangeRole(e.target.value)}
+          onChange={(e) => changeRole(e.target.value)}
           required
         >
           <option value="patient">Patient</option>
@@ -35,7 +52,7 @@ function Register() {
           className="form-control"
           type="text"
           value={name}
-          onChange={(e) => ChangeName(e.target.value)}
+          onChange={(e) => changeName(e.target.value)}
           placeholder="Enter your name"
           required
         />
@@ -47,7 +64,7 @@ function Register() {
           className="form-control"
           type="email"
           value={email}
-          onChange={(e) => ChangeEmail(e.target.value)}
+          onChange={(e) => changeEmail(e.target.value)}
           placeholder="Enter your email address"
           required
         />
@@ -58,62 +75,84 @@ function Register() {
         <input
           className="form-control"
           type="tel"
+          pattern="[0-9]+"
+          placeholder="0712345678"
           value={contact}
-          onChange={(e) => ChangeContact(e.target.value)}
+          onChange={(e) => changeContact(e.target.value)}
           required
         />
       </div>
+      {/**file upload for pharmacy and hospital */}
+      {role != "patient" && (
+        <div className="mb-3">
+          <label className="form-label">
+            Upload documents verifying your institution
+          </label>
+          <input
+            className="form-control"
+            type="file"
+            accept=".pdf, .docx"
+            onChange={(e) => changeFile(e.target.files)}
+            placeholder="Upload files. pdf/docx"
+            required
+          />
+        </div>
+      )}
+      {/**input fields to be hidden if role is not patient */}
+      {role == "patient" && (
+        <>
+          <div className="mb-3">
+            <label className="form-label">Date of birth</label>
+            <input
+              className="form-control"
+              type="date"
+              value={birthday}
+              onChange={(e) => changeBirthday(e.target.value)}
+              required
+            />
+          </div>
 
-      <div className="mb-3">
-        <label className="form-label">Date of birth</label>
-        <input
-          className="form-control"
-          type="date"
-          value={birthday}
-          onChange={(e) => ChangeBirthday(e.target.value)}
-          required
-        />
-      </div>
+          <div className="mb-3">
+            <label className="form-label">Gender</label>
+            <select
+              className="form-select"
+              value={gender}
+              onChange={(e) => changeGender(e.target.value)}
+              required
+            >
+              <option value="" disabled>
+                --Select your gender--
+              </option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="trans">Trans</option>
+              <option value="other">Other</option>
+            </select>
+          </div>
 
-      <div className="mb-3">
-        <label className="form-label">Gender</label>
-        <select
-          className="form-select"
-          value={gender}
-          onChange={(e) => ChangeGender(e.target.value)}
-          required
-        >
-          <option value="" disabled selected>
-            --Select your gender--
-          </option>
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-          <option value="trans">Trans</option>
-          <option value="other">Other</option>
-        </select>
-      </div>
+          <div>
+            <label className="form-label">Password</label>
+            <input
+              className="form-control"
+              type="password"
+              value={pwd}
+              onChange={(e) => changePwd(e.target.value)}
+              required
+            />
+          </div>
 
-      <div>
-        <label className="form-label">Password</label>
-        <input
-          className="form-control"
-          type="password"
-          value={pwd}
-          onChange={(e) => ChangePwd(e.target.value)}
-          required
-        />
-      </div>
-
-      <div>
-        <label className="form-label">Confirm password</label>
-        <input
-          className="form-control"
-          type="password"
-          value={confirmPwd}
-          onChange={(e) => ChangeConfirmPwd(e.target.value)}
-          required
-        />
-      </div>
+          <div>
+            <label className="form-label">Confirm password</label>
+            <input
+              className="form-control"
+              type="password"
+              value={confirmPwd}
+              onChange={(e) => changeConfirmPwd(e.target.value)}
+              required
+            />
+          </div>
+        </>
+      )}
       <p>
         Already have an account?
         <Link className="navbar-brand" to="/login">
